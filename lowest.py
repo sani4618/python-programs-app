2d514610a54d9d51bdacafa76001eaf95459d349
sani4618 <sanijoseph2018@gmail.com>
2023-08-11 08:03:10 +0530

    lowest part is completed

---
+++ lowest.py
@@ -0,0 +1,6 @@
+num1=int(input("enter the First number"))
+num2=int(input("Enter the 2nd number"))
+if num1<num2:
+    print("Smallest number is ",num1)
+else:
+    print("Smallest number is",num2)    
