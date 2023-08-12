6615408e16eb5a8ff32da26a34db91723443cc6c
sani4618 <sanijoseph2018@gmail.com>
2023-08-12 10:17:10 +0530

    biggest

---
+++ func.py
@@ -0,0 +1,9 @@
+def add(x,y):
+    z=x+y
+    return z
+
+
+num1=23
+num2=45
+result=add(num1,num2)
+print(result)
--- multi.py
+++ multi.py
@@ -1,4 +1,7 @@
+def multi(x,y):
+    z=x*y
+    return z
 num1=int(input("enter the first number "))
 num2=int(input("Enter the 2nd number"))
-result=num1*num2
+result=multi(num1,num2)
 print(result)
