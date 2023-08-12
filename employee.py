6eee7b1f61c71452c4c8e2f1c8ade5cfd44ef703
sani4618 <sanijoseph2018@gmail.com>
2023-08-12 09:35:18 +0530

    completd

---
+++ employee.py
@@ -0,0 +1,7 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+compname=input("Enter your company name: ")
+phnum=int(input("Enter your phone num: "))
+emailId=input("Enter your emailid: ")
+emp={"name":name,"Age":age,"compname":compname,"Phone Number":phnum,"EmailId":emailId}
+print(emp)
